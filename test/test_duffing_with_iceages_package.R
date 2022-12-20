05404a1ecf64af72a08dbe84f786d4a07c664734
mcrucifix <michel.crucifix@uclouvain.be>
2022-12-20 16:04:15 +0100

    test duffing vanderpol

--- test/test_duffing_with_iceages_package.R
+++ test/test_duffing_with_iceages_package.R
@@ -1,7 +1,6 @@
 require(iceages)
 data(models)
-
-require(mtm)
+require(multitaper)
 times = seq(-500,0,0.1)
 deltat = c(0.001, 0.01, 0.1)
 
@@ -24,16 +23,38 @@ model = models$duffing_vdp_d
 #model$spar['omega'] = 1.0
 
 sol = list()
-for (i in seq(along=deltat))
- {
-  sol[[i]] <- propagate_d (model, model$spar, init=c(0.1,0.1), 
-                          times=times, deltat=deltat[i], Astro=Astro)
- }
+
+kappas=seq(-1,2,0.1)
+
+deltat = 0.1
+sols <- lapply(kappas,
+         function(kappa){
+            spar <- model$spar
+            spar['kappa'] <- kappa
+            print(spar)
+            propagate_d (model, spar, init=c(0.1,0.1), 
+                             times=times, deltat=deltat, Astro=Astro)
+})
 
 i=0
 # plot(times[-1],diff(sol[[1]][,2]), type='l', xlim=c(-20,-10), ylim=c(-4,4))
 # lines(times[-1],diff(sol[[1]][,1]), type='l', xlim=c(-20,-10), ylim=c(-4,4))
 
-plot(times/100,sol[[1]][,1], type='l', xlim=c(-1,0))
-lines(times/100,sol[[2]][,1], type='l', col='red')
+plot(times/100,sols[[1]][,1], type='l', xlim=c(-1,0))
+lapply(sols, function(sol) lines(times/100,sol[,1], type='l', col='red'))
 #lines(times/100,sol[[3]][,1], type='l', col='blue')
+
+spec <- lapply(sols, function(sol)
+        Mod(fft(sol[,1]))[-1][0:1000]) 
+
+
+freqs <- 1./(length(times)*deltat)*seq(1000)
+periods <- 1./freqs * 10     # 1 time unit = 10ka
+
+
+AmpMax <- sapply(spec, max) 
+PerMax <- periods[sapply(spec, which.max)]
+
+
+
+
